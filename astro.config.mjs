3993c925209b7b6b4ac5fc5b3df3be81ddcb977f
0RDNRY <sidsw6rk@gmail.com>
2026-07-10 10:42:13 +0530

    Fix Astro base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://0RDNRY.github.io",
-  base: "/sid-web"
+  site: "https://0rdnry.github.io",
+  base: "/sid-web/"
 });
